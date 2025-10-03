7c151e779d0e0997e5dcac2847a3d0887ce0d606
renttelus <telusrent@gmail.com>
2025-10-03 14:27:03 -0500

    UnitTest1.cs

---
+++ ProyectoDevOps_WilsonCorreal.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ProyectoDevOps_WilsonCorreal.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SumaBasica()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
