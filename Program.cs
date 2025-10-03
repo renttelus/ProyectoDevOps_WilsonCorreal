3b201447e6fae637629b70526ee840509ac17c22
renttelus <telusrent@gmail.com>
2025-10-03 14:23:48 -0500

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola, soy Wilson Correal y este es mi primer proyecto DevOps con GitHub Actions");
+    }
+}
